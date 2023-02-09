dba451017755674abff57ce2e797c0b4578da026
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-02-09 19:25:11 +0500

    Добавила задачу 29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт м
+// ассив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+// Console.Write("Введите число ");
+// int n = int.Parse (Console.ReadLine());
+int n = 8;
+int [] array = GetDecArray(n);
+Console.WriteLine($"[{String.Join(", ", array)}]"); // у библиотеки string есть метод Join
+// он объединяет массив array строкой которую введем в параметр (", ")
+int [] GetDecArray (int n)
+{
+  int [] result = new int [n]; // создали массив на size элементов
+  for (int i = 0; i < n; i++)
+  {
+    result [i] = new Random().Next(100); // на каждый элемент массива кладем рандомное число2 значит будет 0 и 1
+  }
+  return result;
+}
