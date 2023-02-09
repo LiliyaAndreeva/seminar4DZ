ce406a4b4f5e4fc42351a0c28ebe1a33c2f12a6d
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-02-09 19:23:08 +0500

    Добавила задачу 25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит 
+//число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число  ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} ^ {B} = {GetPower(A)}");
+
+int GetPower (int A)
+{
+ int Power = 1;
+ for (int i=1; i<=B; i++)
+ {
+  Power *= A;
+ }
+ return Power;
+}  
+ 
